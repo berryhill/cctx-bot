0bfec0fbfde7fc8027097236984a6346f748e7fb
Matthew Berryhill <matthewberryhill@gmail.com>
2025-11-10 14:11:41 -0500

    Replace index.html route with health check endpoint

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const streamPrivate = require('./wss/wss_auth_md');
 const ExpressServer = require('./express/express');
 const express = new ExpressServer();
 const stream = new BitmexStream(false);
-const path = require('path');
 
 //Loggers and Color
 const color = require('./utils/colors')
@@ -1727,8 +1726,8 @@ async function main(app) {
     //GET LINKS FROM EXPRESS
     //GET - React Index.js
     app.get('/', async function (req, res) {
-        res.sendFile(path.resolve(__dirname+"./build/public/index.html"));
-        expressLog.print('Request','/ requested')
+        res.status(200).json({ status: 'ok', service: 'ccxt-bot' });
+        expressLog.print('Request','/ health check')
     })
 
     //API LINKS
